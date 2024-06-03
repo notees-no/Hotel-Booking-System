e21d1ec044903e400362531e799caad26d1ff207
notees-no <skrebloegor@gmail.com>
2024-06-03 21:49:08 +0300

    ...

--- hbsrapi.sql
+++ hbsrapi.sql
@@ -19,9 +19,9 @@ CREATE TABLE IF NOT EXISTS `hotels` (
 );
 
 -- Заполнение таблицы hotels
-INSERT INTO hotels (name, hotelPhone, hotelType, role)
-VALUES ('Hotel A', '123456789', 'HOTEL', 'ROLE_ADMIN'),
-       ('Hotel B', '987654321', 'HOTEL', 'ROLE_USER');
+INSERT INTO hotels (hotel_id, name, hotelPhone, hotelType, role)
+VALUES (1, 'Hotel A', '123456789', 'HOTEL', 'ROLE_ADMIN'),
+       (2, 'Hotel B', '987654321', 'HOTEL', 'ROLE_USER');
 
 -- Создание таблиц users, rooms, reservations
 CREATE TABLE IF NOT EXISTS `users` (
@@ -80,16 +80,16 @@ CREATE TABLE IF NOT EXISTS `reservations` (
 );
 
 -- Заполнение таблиц users, rooms, reservations
-INSERT INTO users (username, password, role)
-VALUES ('admin', '$2a$10$4ntneHfvOYZFg8JPeHxh1.S89KlFsmRN/Gu0VWIZ/8ZeqIVUIBMLC', 'ROLE_ADMIN'),
-       ('user1', '$2a$10$BnXIcgFcXjCn0oC4atAVP.bIAk.HUE4y/oK.BW/awcGO8.trcX5XK', 'ROLE_USER');
+INSERT INTO users (id, username, password, role)
+VALUES (1, 'admin', '$2a$10$4ntneHfvOYZFg8JPeHxh1.S89KlFsmRN/Gu0VWIZ/8ZeqIVUIBMLC', 'ROLE_ADMIN'),
+       (2, 'user1', '$2a$10$BnXIcgFcXjCn0oC4atAVP.bIAk.HUE4y/oK.BW/awcGO8.trcX5XK', 'ROLE_USER');
 
-INSERT INTO rooms (roomNumber, roomType, noOfPerson, price, available, hotel_hotel_id)
-VALUES (101, 'AC', 2, 100.00, true, 1),
-       (102, 'NON_AC', 2, 80.00, true, 1),
-       (201, 'AC', 3, 150.00, true, 2),
-       (202, 'AC', 3, 150.00, true, 2);
+INSERT INTO rooms (room_id, roomNumber, roomType, noOfPerson, price, available, hotel_hotel_id)
+VALUES (1, 101, 'AC', 2, 100.00, true, 1),
+       (2, 102, 'NON_AC', 2, 80.00, true, 1),
+       (3, 201, 'AC', 3, 150.00, true, 2),
+       (4, 202, 'AC', 3, 150.00, true, 2);
 
-INSERT INTO reservations (checkinDate, checkoutDate, noOfPerson, status, room_room_id, hotel_hotel_id, user_id)
-VALUES ('2024-06-10', '2024-06-15', 2, 'BOOKED', 1, 1, 1),
-       ('2024-06-20', '2024-06-25', 3, 'BOOKED', 3, 2, 2);
+INSERT INTO reservations (reservation_id, checkinDate, checkoutDate, noOfPerson, status, room_room_id, hotel_hotel_id, user_id)
+VALUES (1, '2024-06-10', '2024-06-15', 2, 'BOOKED', 1, 1, 1),
+       (2, '2024-06-20', '2024-06-25', 3, 'BOOKED', 3, 2, 2);
