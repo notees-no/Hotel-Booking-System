0482dda2912cd9c9d7ff629fdefdd1e6238cab55
notees-no <skrebloegor@gmail.com>
2024-06-04 17:11:11 +0300

    ...

--- src/main/java/com/service/JwtService.java
+++ src/main/java/com/service/JwtService.java
@@ -19,7 +19,7 @@ import java.util.function.Function;
 
 @Service
 public class JwtService {
-    @Value("${token.signing.key}")
+    @Value("53A73E5F1C4E0A2D3B5F2D784E6A1B423D6F247D1F6E5C3A596D635A75327855")
     private String jwtSigningKey;
 
     /**
